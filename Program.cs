4bfbf27b81e6697d79b72313041004cd7658457c
Felix <felixmcwong+Github@gmail.com>
2025-10-22 18:47:08 +1300

    yes

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace pathfinding
+﻿using static pathfinding.Program;
+
+namespace pathfinding
 {
     internal class Program
     {
@@ -16,8 +18,8 @@
             }
         }
 
-        public Tile WallTile = new Tile('#', false, ConsoleColor.White);
-        public Tile FloorTile = new Tile('.', true, ConsoleColor.Gray);
+        public static readonly Tile WallTile = new Tile('#', false, ConsoleColor.White);
+        public static readonly Tile FloorTile = new Tile('·', true, ConsoleColor.Gray);
 
         public class Entity
         {
@@ -40,10 +42,38 @@
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.CursorVisible = false;
+
+            Tile[,] map = GenerateCaves(Console.WindowWidth, Console.WindowHeight);
+
+            DrawSimple(map);
+
+            while (true)
+            {
+            }
         }
 
-        public Tile[,] GenerateCaves(int width, int height)
+        // (Assuming you have: Tile[,] map, Player player, List<Enemy> enemies)
+
+        public static void DrawSimple(Tile[,] map)
+        {
+            // 1. Draw the map
+            // We set the cursor once at the top-left to avoid flicker
+            Console.SetCursorPosition(0, 0);
+
+            for (int y = 0; y < map.GetLength(1); y++) // map.GetLength(1) is height
+            {
+                for (int x = 0; x < map.GetLength(0); x++) // map.GetLength(0) is width
+                {
+                    Tile tile = map[x, y];
+                    Console.ForegroundColor = tile.Color;
+                    Console.Write(tile.DisplayChar);
+                }
+                if (y != map.GetLength(1) - 1) Console.WriteLine();
+            }
+        }
+
+        public static Tile[,] GenerateCaves(int width, int height)
         {
             Tile[,] map = new Tile[width, height];
             Random rand = new Random();
@@ -65,13 +95,51 @@
 
             for (int i = 0; i < 5; i++)
             {
+                Tile[,] newMap = (Tile[,])map.Clone();
 
+                for (int x = 0; x < width - 1; x++)
+                {
+                    for (int y = 0; y < height - 1; y++)
+                    {
+                        int wallNeighbours = CountWallNeighbours(map, x, y);
+
+                        if (!map[x, y].IsWalkable)
+                        {
+                            if (wallNeighbours < 4) newMap[x, y] = FloorTile;
+                        }
+                        else
+                        {
+                            if (wallNeighbours > 4) newMap[x, y] = WallTile;
+                        }
+                    }
+                }
+
+                map = newMap;
             }
+
+            return map;
         }
 
-        int CountWallNeighbours(Tile[,] map, int x, int y)
+        public static int CountWallNeighbours(Tile[,] map, int x, int y)
         {
+            int count = 0;
+            for (int i = -1; i <= 1; i++)
+            {
+                for (int j = -1; j <= 1; j++)
+                {
+                    if (i == 0 && j == 0) continue;
 
+                    try
+                    {
+                        if (!map[x + i, y + j].IsWalkable)
+                        {
+                            count++;
+                        }
+                    }
+                    catch { }
+                }
+            }
+            return count;
         }
     }
 }
