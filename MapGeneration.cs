5b4609c2787aa49c5bcd0544fe6250ffad6f2ea8
Felix <felixmcwong+Github@gmail.com>
2025-10-23 19:22:29 +1300

    moving houses

---
+++ MapGeneration.cs
@@ -0,0 +1,99 @@
+﻿using static pathfinding.Program;
+
+namespace pathfinding
+{
+    internal class MapGeneration
+    {
+        public static Tile[,] Generate(int width, int height)
+        {
+            Map = new Tile[width, height];
+            MapBuffer = new Tile[width, height];
+            Random rand = new Random();
+
+            Tile empty = new Tile(' ', false, ConsoleColor.Black);
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    MapBuffer[x, y] = empty;
+                }
+            }
+
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    if (x == 0 || x == width - 1 || y == 0 || y == height - 1)
+                    {
+                        Map[x, y] = WallTile;
+                    }
+                    else
+                    {
+                        Map[x, y] = rand.Next(0, 100) < 45 ? WallTile : FloorTile;
+                    }
+                }
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Tile[,] newMap = (Tile[,])Map.Clone();
+
+                for (int x = 0; x < width - 1; x++)
+                {
+                    for (int y = 0; y < height - 1; y++)
+                    {
+                        int wallNeighbours = CountWallNeighbours(x, y);
+
+                        if (!Map[x, y].IsWalkable)
+                        {
+                            if (wallNeighbours < 4) newMap[x, y] = FloorTile;
+                        }
+                        else
+                        {
+                            if (wallNeighbours > 4) newMap[x, y] = WallTile;
+                        }
+                    }
+                }
+
+                Map = newMap;
+            }
+
+            /*
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    if (x == 0 || x == width - 1 || y == 0 || y == height - 1)
+                    {
+                        Map[x, y] = WallTile;
+                    }
+                }
+            }
+            */
+
+            return Map;
+        }
+
+        public static int CountWallNeighbours(int x, int y)
+        {
+            int count = 0;
+            for (int i = -1; i <= 1; i++)
+            {
+                for (int j = -1; j <= 1; j++)
+                {
+                    if (i == 0 && j == 0) continue;
+
+                    try
+                    {
+                        if (!Map[x + i, y + j].IsWalkable)
+                        {
+                            count++;
+                        }
+                    }
+                    catch { }
+                }
+            }
+            return count;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,12 +10,15 @@ namespace pathfinding
             public char DisplayChar;
             public bool IsWalkable;
             public ConsoleColor Color;
+            public bool IsDiscovered;
+            public bool IsInView;
 
             public Tile(char displayChar, bool isWalkable, ConsoleColor color)
             {
                 DisplayChar = displayChar;
                 IsWalkable = isWalkable;
                 Color = color;
+                IsDiscovered = false;
             }
         }
 
@@ -52,7 +55,7 @@ namespace pathfinding
             using var cts = new CancellationTokenSource();
             var spinnerTask = Task.Run(() => Throb(cts.Token));
 
-            Map = GenerateCaves(Console.WindowWidth, Console.WindowHeight);
+            Map = MapGeneration.Generate(Console.WindowWidth, Console.WindowHeight);
 
             cts.Cancel();
             spinnerTask.Wait();
@@ -165,96 +168,6 @@ namespace pathfinding
                 }
                 if (y != Map.GetLength(1) - 1) Console.WriteLine();
             }
-        }
-
-        public static Tile[,] GenerateCaves(int width, int height)
-        {
-            Map = new Tile[width, height];
-            MapBuffer = new Tile[width, height];
-            Random rand = new Random();
-
-            Tile empty = new Tile(' ', false, ConsoleColor.Black);
-            for (int x = 0; x < width; x++)
-            {
-                for (int y = 0; y < height; y++)
-                {
-                    MapBuffer[x, y] = empty;
-                }
-            }
-
-            for (int x = 0; x < width; x++)
-            {
-                for (int y = 0; y < height; y++)
-                {
-                    if (x == 0 || x == width - 1 || y == 0 || y == height - 1)
-                    {
-                        Map[x, y] = WallTile;
-                    }
-                    else
-                    {
-                        Map[x, y] = rand.Next(0, 100) < 45 ? WallTile : FloorTile;
-                    }
-                }
-            }
-
-            for (int i = 0; i < 5; i++)
-            {
-                Tile[,] newMap = (Tile[,])Map.Clone();
-
-                for (int x = 0; x < width - 1; x++)
-                {
-                    for (int y = 0; y < height - 1; y++)
-                    {
-                        int wallNeighbours = CountWallNeighbours(x, y);
-
-                        if (!Map[x, y].IsWalkable)
-                        {
-                            if (wallNeighbours < 4) newMap[x, y] = FloorTile;
-                        }
-                        else
-                        {
-                            if (wallNeighbours > 4) newMap[x, y] = WallTile;
-                        }
-                    }
-                }
-
-                Map = newMap;
-            }
-
-            //for (int x = 0; x < width; x++)
-            //{
-            //    for (int y = 0; y < height; y++)
-            //    {
-            //        if (x == 0 || x == width - 1 || y == 0 || y == height - 1)
-            //        {
-            //            Map[x, y] = WallTile;
-            //        }
-            //    }
-            //}
-
-            return Map;
-        }
-
-        public static int CountWallNeighbours(int x, int y)
-        {
-            int count = 0;
-            for (int i = -1; i <= 1; i++)
-            {
-                for (int j = -1; j <= 1; j++)
-                {
-                    if (i == 0 && j == 0) continue;
-
-                    try
-                    {
-                        if (!Map[x + i, y + j].IsWalkable)
-                        {
-                            count++;
-                        }
-                    }
-                    catch { }
-                }
-            }
-            return count;
         }
     }
 }
